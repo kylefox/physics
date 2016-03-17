2af474f22b61a703b800c09be29bc61e078b556a
Kyle Fox <kylefox@gmail.com>
2016-03-17 14:39:41 -0600

    sum arbitrary vectors

--- src/Vector3f.js
+++ src/Vector3f.js
@@ -1,5 +1,23 @@
 export default class Vector3f {
 
+  static add(v1, v2) {
+    return new Vector3f(
+      v1.x + v2.x,
+      v1.y + v2.y,
+      v1.z + v2.z)
+    ;
+  }
+
+  static sum(...vectors) {
+    let x=0, y=0, z=0;
+    vectors.forEach((vector) => {
+      x += vector.x;
+      y += vector.y;
+      z += vector.z;
+    });
+    return new Vector3f(x, y, z);
+  }
+
   constructor(x, y, z) {
     this.x = x;
     this.y = y;
--- test/Vector3f.spec.js
+++ test/Vector3f.spec.js
@@ -2,22 +2,46 @@ import Vector3f from '../src/Vector3f';
 
 describe('Vector3f', () => {
 
-  it('should be initialized with x, y, z components', () => {
-  	const vector = new Vector3f(1, 2, 3);
-    expect(vector.x).toEqual(1.0);
-    expect(vector.y).toEqual(2.0);
-    expect(vector.z).toEqual(3.0);
+  describe('instances', () => {
+    it('should be initialized with x, y, z components', () => {
+    	const vector = new Vector3f(1, 2, 3);
+      expect(vector.x).toEqual(1.0);
+      expect(vector.y).toEqual(2.0);
+      expect(vector.z).toEqual(3.0);
+    });
+
+    it('should have a magnitude (length)', () => {
+      const vector = new Vector3f(1, 2, 3);
+      expect(vector.magnitude()).toEqual(3.7416573867739413);
+    });
+
+    it('can be normalized', () => {
+      const vector = new Vector3f(1, 2, 3);
+      vector.normalize();
+      expect(vector.magnitude()).toEqual(1.0);
+    });
   });
 
-  it('should have a magnitude (length)', () => {
-    const vector = new Vector3f(1, 2, 3);
-    expect(vector.magnitude()).toEqual(3.7416573867739413);
+  it('can add vectors', () => {
+    const vector = Vector3f.add(
+      new Vector3f(1, 2, 3),
+      new Vector3f(2, 4, 6)
+    );
+    expect(vector.x).toEqual(3);
+    expect(vector.y).toEqual(6);
+    expect(vector.z).toEqual(9);
   });
 
-  it('can be normalized', () => {
-    const vector = new Vector3f(1, 2, 3);
-    vector.normalize();
-    expect(vector.magnitude()).toEqual(1.0);
+  it('can sum an arbitrary number of vectors', () => {
+    const vector = Vector3f.sum(
+      new Vector3f(1, 2, 3),
+      new Vector3f(2, 4, 6),
+      new Vector3f(0, -5, 8),
+      new Vector3f(-9, 15, 12),
+    );
+    expect(vector.x).toEqual(-6);
+    expect(vector.y).toEqual(16);
+    expect(vector.z).toEqual(29);
   });
 
 });
